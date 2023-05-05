455fced0d8a493998ee5de957a9f14aa73c70c87
Dirk Breeuwer <departdisclose@protonmail.com>
2023-05-05 05:41:43 +0000

    Add identify API calls - Partially working solution

--- website_analysis/network_analysis.py
+++ website_analysis/network_analysis.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import json
 import re
+import pdb
 
 class NetworkAnalyzer:
     def __init__(self, url, user_requirements, captured_traffic=None):
@@ -22,9 +23,9 @@ class NetworkAnalyzer:
             # Enable headless mode (optional)
             chrome_options.add_argument('--headless')
             # Disable images (optional, for faster browsing)
-            chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+            #chrome_options.add_argument('--blink-settings=imagesEnabled=false')
             # Disable GPU (optional, may improve stability)
-            chrome_options.add_argument('--disable-gpu')
+            #chrome_options.add_argument('--disable-gpu')
 
             # Initialize the browser with the specified capabilities and options
             browser = webdriver.Chrome(desired_capabilities=desired_capabilities, options=chrome_options)
@@ -34,7 +35,18 @@ class NetworkAnalyzer:
 
             # Extract network traffic logs
             logs = browser.get_log('performance')
+
+            # Save the longs to a text file
+            with open('logs.txt', 'w') as f:
+                f.write(str(logs))
+
             self.captured_traffic = [json.loads(log['message'])['message'] for log in logs]
+
+            # Save the captured traffic to a JSON file
+
+            with open('captured_traffic.json', 'w') as f:
+                json.dump(self.captured_traffic, f)
+
 
             # Close the browser
             browser.quit()
@@ -47,19 +59,35 @@ class NetworkAnalyzer:
 
 
     def identify_api_calls(self):
-        api_calls = []
+        identified_api_calls = []
 
-        for request in self.captured_traffic:
-            if self._is_api_call(request):
-                api_call = {
-                    "url": request["url"],
-                    "method": request["method"],
-                    "request_headers": request["headers"],
-                    "response_headers": request["response"]["headers"],
-                }
-                api_calls.append(api_call)
+        for entry in self.captured_traffic:
+            method = entry.get('method')
 
-        self.analysis_results["api_calls"] = api_calls
+            if method == 'Network.requestWillBeSent':
+                request = entry['params']['request']
+                request_url = request['url']
+                request_method = request['method']
+                content_type = request['headers'].get('Content-Type', '')
+
+                # Check if the request is an AJAX request
+                if request_method in ['GET', 'POST'] and ('application/json' in content_type or 'application/xml' in content_type):
+                    api_call = {
+                        'url': request_url,
+                        'request': request
+                    }
+                    identified_api_calls.append(api_call)
+
+                # Check if the request is a WebSocket connection
+                elif 'WebSocket' in request['headers'].get('Upgrade', ''):
+                    api_call = {
+                        'url': request_url,
+                        'request': request
+                    }
+                    identified_api_calls.append(api_call)
+
+        # Store the identified API calls in the api_calls attribute
+        self.api_calls = identified_api_calls
 
     def _is_api_call(self, request):
         content_type = request["response"]["headers"].get("Content-Type", "").lower()
@@ -67,20 +95,62 @@ class NetworkAnalyzer:
 
 
     def analyze_api_calls(self):
-        # Placeholder for analyzing API calls
-        pass
+        # Return the list of identified API calls
+        return self.api_calls
 
     def filter_api_calls(self):
         # Placeholder for filtering API calls based on user requirements
         pass
 
     def get_analysis_results(self):
-        return self.api_calls
+        # Call analyze_api_calls and return the results
+        return self.analyze_api_calls()
+
+def main(url):
+    user_requirements = {}
+    analyzer = NetworkAnalyzer(url, user_requirements)
+    analyzer.capture_network_traffic()
+    analyzer.identify_api_calls()
+    return analyzer.get_analysis_results()
+
+
+def analyze_websites(websites):
+    results = {}
+    for url in websites:
+        try:
+            print(f"Analyzing {url}...")
+            analysis_results = main(url)
+            results[url] = analysis_results
+            print(f"Analysis results for {url}: {analysis_results}")
+        except Exception as e:
+            print(f"Error analyzing {url}: {e}")
+            results[url] = {"error": str(e)}
+    return results
+
 
 # Usage example
 if __name__ == "__main__":
-    url = "https://example.com"
-    user_requirements = {}
-    analyzer = NetworkAnalyzer(url, user_requirements)
-    analyzer.identify_api_calls()
-    print(analyzer.get_analysis_results())
+    websites = [
+        "https://www.airbnb.com",
+        "https://www.twitter.com",
+        "https://www.github.com",
+        "https://www.reddit.com",
+        "https://www.spotify.com",
+        "https://www.netflix.com",
+        "https://www.amazon.com",
+        "https://www.instagram.com",
+        "https://www.linkedin.com",
+        "https://www.trello.com",
+        "https://www.slack.com",
+        "https://www.pinterest.com",
+        "https://www.foursquare.com",
+        "https://www.yelp.com",
+        "https://www.dropbox.com",
+        "https://www.medium.com",
+        "https://www.coursera.org",
+        "https://www.stackoverflow.com",
+        "https://www.khanacademy.org",
+        "https://www.weather.com",
+    ]
+    results = analyze_websites(websites)
+    print(results)
